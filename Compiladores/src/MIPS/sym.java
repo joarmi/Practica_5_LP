ef690382ce96ac5438d60b6f10e2a24ec58d06ca
joarmi <javiondogamer@gmail.com>
2016-11-30 12:09:38 +0100

    Estado Inicial
    Estado inicial de la practica

---
+++ Compiladores/src/MIPS/sym.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Javier Argente Micó
+ */
+package MIPS;
+public class sym {
+    
+    public static final int EOF = 0;
+    public static final int COM_ANIDADO = 1;
+    public static final int COM = 2;
+    public static final int MOSTRAR = 3;
+    public static final int LEER = 4;
+    public static final int PUNTO_COMA = 5;
+    public static final int ASIGNACION = 6;
+    public static final int MULTIPLICACION = 7;
+    public static final int DIVISION = 8;
+    public static final int SUMA = 9;
+    public static final int RESTA = 10;
+    public static final int VARIABLE_VALIDA = 11;
+    public static final int ESPACIO_BLANCO = 12;
+    public static final int FIN_LINEA = 13;
+    
+}
